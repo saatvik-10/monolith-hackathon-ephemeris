3b525fb25c708e4ed000a5fca82f9aca09e2035a
saatvik-10 <saatvikmadan529@gmail.com>
2026-02-15 14:26:59 +0530

    added attendance controller

---
+++ server/src/controllers/attendance.controller.ts
@@ -0,0 +1,39 @@
+import type { Context } from 'hono';
+import { prisma } from '../../prisma';
+
+export class Attendance {
+  async markAttendance(ctx: Context) {
+    const identity = ctx.get('identity');
+
+    if (identity.attendedAt) {
+      return ctx.json('Attendance already marked', 409);
+    }
+
+    try {
+      const now = new Date();
+
+      const updatedIdentity = await prisma.identity.update({
+        where: { id: identity.id },
+        data: { attendedAt: now },
+      });
+
+      await prisma.proof.create({
+        data: {
+          type: 'ATTENDANCE',
+          identityId: updatedIdentity.id,
+          eventId: updatedIdentity.eventId,
+          issuedAt: now,
+          expiresAt: updatedIdentity.expiresAt,
+          payload: {
+            eventId: updatedIdentity.eventId,
+            attendedAt: now.toISOString(),
+          },
+          signature: '', //with web3 integration
+        },
+      });
+    } catch (err) {
+      console.log('Err marking attendance', err);
+      return ctx.json('Err marking attendance', 500);
+    }
+  }
+}
--- server/src/routes/attendance.route.ts
+++ server/src/routes/attendance.route.ts
@@ -1,7 +1,9 @@
 import { Hono } from 'hono';
+import { Attendance } from '../controllers/attendance.controller';
 
 const attendanceRoute = new Hono();
+const controller = new Attendance();
 
-attendanceRoute.post('/mark');
+attendanceRoute.post('/mark', controller.markAttendance);
 
 export { attendanceRoute };
