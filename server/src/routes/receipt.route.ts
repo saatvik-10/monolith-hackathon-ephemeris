8b6fe5fff1f6c8d7f003f5d26972a43c276199e9
saatvik-10 <saatvikmadan529@gmail.com>
2026-02-15 16:57:01 +0530

    added proxy

--- server/src/routes/receipt.route.ts
+++ server/src/routes/receipt.route.ts
@@ -5,7 +5,7 @@ import { proxy } from '../proxy';
 const receiptRoute = new Hono();
 const controller = new Receipt();
 
-receiptRoute.post('/issue', controller.issueReceipt);
+receiptRoute.post('/issue', proxy, controller.issueReceipt);
 receiptRoute.get('', proxy, controller.getReceipts);
 
 export { receiptRoute };
