a470d37dbfb8237edd65dcdc3b0528a5ce004267
saatvik-10 <saatvikmadan529@gmail.com>
2026-02-26 11:35:01 +0530

    udpated event validator

--- server/src/validators/event.validator.ts
+++ server/src/validators/event.validator.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 export const eventSchema = z.object({
   name: z.string().min(3, 'Event name should be atleast 3 characters long'),
+  image: z.string('Event image is required'),
   startTime: z.date(),
   endTime: z.date(),
   expiryWindow: z.number(),
