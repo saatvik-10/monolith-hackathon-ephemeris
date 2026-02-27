6554d0e8cf2e8bc1b01009128043bd271e6bec03
saatvik-10 <saatvikmadan529@gmail.com>
2026-02-27 10:08:08 +0530

    nft minter controller

--- server/src/controllers/receipt.controller.ts
+++ server/src/controllers/receipt.controller.ts
@@ -1,8 +1,12 @@
 import type { Context } from 'hono';
 import { prisma } from '../../prisma';
-import { receiptSchema } from '../validators/receipt.validator';
+import { nftMintSchema, receiptSchema } from '../validators/receipt.validator';
 import { signProof } from '../lib/signature';
-
+import { generateContentHash } from '../lib/hash';
+import { mintNFT } from '../utils/mint';
+import { uploadToIPFS } from '../utils/ipfs';
+import { generateMetadata, validateMetadata } from '../utils/metadata';
+import { success } from 'zod';
 export class Receipt {
   async issueReceipt(ctx: Context) {
     const data = receiptSchema.safeParse(await ctx.req.json());
@@ -75,6 +79,100 @@ export class Receipt {
     } catch (err) {
       console.log('Err fetching receipts', err);
       return ctx.json('Err fetching receipts', 500);
+    }
+  }
+
+  async mintNFTForReceipt(ctx: Context) {
+    const identity = ctx.get('identity');
+
+    if (!identity) {
+      return ctx.json('No identity found', 401);
+    }
+
+    const receiptId = ctx.req.param('receipt_id');
+    const data = nftMintSchema.safeParse(await ctx.req.json());
+
+    const walletAddress = data.data?.walletAddress;
+
+    if (!data.success) {
+      return ctx.json('Invalid input', 422);
+    }
+
+    try {
+      const receipt = await prisma.receipt.findUnique({
+        where: {
+          id: receiptId,
+        },
+        include: { event: true, identity: true },
+      });
+
+      if (!receipt) {
+        return ctx.json('Receipt not found', 404);
+      }
+
+      if (!receipt.identityId != identity.id) {
+        return ctx.json('Receipt does not belong to the user', 403);
+      }
+
+      if (receipt.nftMinted) {
+        return ctx.json('NFT already minted for this receipt', 409);
+      }
+
+      const contentHash = generateContentHash(
+        receiptId,
+        receipt.eventId,
+        Math.floor(new Date().getTime() / 1000),
+        process.env.PROOF_SECRET!,
+      );
+
+      const metadata = generateMetadata(
+        receipt.event.name,
+        receipt.event.startTime.toISOString().split('T')[0] as string,
+        contentHash,
+        receipt.event.image,
+        receipt.id,
+        walletAddress as string,
+      );
+
+      if (!validateMetadata(metadata)) {
+        throw new Error('Metadata verification failed');
+      }
+
+      const metadataUri = await uploadToIPFS(metadata);
+
+      const { mintAddress } = await mintNFT(
+        metadataUri,
+        walletAddress as string,
+        receipt.event.name,
+      );
+
+      const updatedReceipt = await prisma.receipt.update({
+        where: {
+          id: receiptId,
+        },
+        data: {
+          contentHash,
+          walletAddress,
+          mintAddress,
+          metadataUri,
+          nftMinted: true,
+          mintedAt: new Date(),
+        },
+      });
+
+      return ctx.json(
+        {
+          receipt: updatedReceipt,
+          nft: {
+            mintAddress,
+            metadataUri,
+          },
+        },
+        201,
+      );
+    } catch (err) {
+      console.error(`Error minting NFT for receipt`, err);
+      return ctx.json('Err minting nft for the receipt', 500);
     }
   }
 }
