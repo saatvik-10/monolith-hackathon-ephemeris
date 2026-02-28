9ae0a3fcc03a5b0f271636dbd7f745f7fb9877bf
saatvik-10 <saatvikmadan529@gmail.com>
2026-02-28 14:28:43 +0530

    updated event validators

--- server/src/validators/event.validator.ts
+++ server/src/validators/event.validator.ts
@@ -2,10 +2,12 @@ import { z } from 'zod';
 
 export const eventSchema = z.object({
   name: z.string().min(3, 'Event name should be atleast 3 characters long'),
-  image: z.string('Event image is required'),
-  startTime: z.date(),
-  endTime: z.date(),
-  expiryWindow: z.number(),
+  image: z.url('Event image must be a valid URL'),
+  startTime: z.iso.datetime('Start time must be ISO datetime'),
+  endTime: z.iso.datetime('End time must be ISO datetime'),
+  organizerWallet: z.string().optional(),
+  nftEnabled: z.boolean().optional().default(false),
+  expiryWindow: z.number().optional().default(360),
 });
 
 export type EventType = z.infer<typeof eventSchema>;
