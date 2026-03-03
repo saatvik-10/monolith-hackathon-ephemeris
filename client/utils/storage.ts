e37cc378d0ab1dc7a55ad02d131a7afad5723635
saatvik-10 <saatvikmadan529@gmail.com>
2026-03-03 23:01:34 +0530

    added async storage

---
+++ client/utils/storage.ts
@@ -0,0 +1,13 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const asyncStorageAdapter = {
+  getItem: async (key: string): Promise<string | null> => {
+    return AsyncStorage.getItem(key);
+  },
+  setItem: async (key: string, value: string): Promise<void> => {
+    await AsyncStorage.setItem(key, value);
+  },
+  removeItem: async (key: string): Promise<void> => {
+    await AsyncStorage.removeItem(key);
+  },
+};
