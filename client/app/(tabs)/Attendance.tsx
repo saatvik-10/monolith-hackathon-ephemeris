72ca30e158eea5a935b0b50d1db13677cf7eeb48
harshmalu9 <hmalu975@gmail.com>
2026-03-03 21:52:57 +0530

    added flatlist for rendering the cards

--- client/app/(tabs)/Attendance.tsx
+++ client/app/(tabs)/Attendance.tsx
@@ -1,40 +1,53 @@
-import { ScrollView, View } from "react-native";
+import { FlatList } from "react-native";
 import ScreenBackground from "../../components/ScreenBackground";
 import AttendanceCard from "../../components/AttendanceCard";
+
+const attendanceData = [
+  {
+    id: "1",
+    name: "Hackathon 2025",
+    date: "12 March 2025",
+    image: "https://picsum.photos/300",
+    NFTEnabled: true,
+  },
+  {
+    id: "2",
+    name: "Web3 Bootcamp",
+    date: "28 February 2025",
+    image: "https://picsum.photos/301",
+    NFTEnabled: false,
+  },
+  {
+    id: "3",
+    name: "AI Workshop",
+    date: "5 January 2025",
+    image: "https://picsum.photos/302",
+    NFTEnabled: true,
+  },
+];
 
 const Attendance = () => {
   return (
     <ScreenBackground>
-      <ScrollView
-        className="flex-1 px-5 pt-6"
-        contentContainerStyle={{ paddingBottom: 120 }}
+      <FlatList
+        data={attendanceData}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingTop: 24,
+          paddingBottom: 120, // space for floating tab bar
+        }}
         showsVerticalScrollIndicator={false}
-      >
-
-        <AttendanceCard
-          name="Hackathon 2025"
-          date="12 March 2025"
-          image="https://picsum.photos/300"
-          NFTEnabled={true}
-          onVerify={() => console.log("Verify pressed")}
-        />
-
-        <AttendanceCard
-          name="Web3 Bootcamp"
-          date="28 February 2025"
-          image="https://picsum.photos/301"
-          NFTEnabled={false}
-        />
-
-        <AttendanceCard
-          name="AI Workshop"
-          date="5 January 2025"
-          image="https://picsum.photos/302"
-          NFTEnabled={true}
-          onVerify={() => console.log("Verify pressed")}
-        />
-
-      </ScrollView>
+        renderItem={({ item }) => (
+          <AttendanceCard
+            name={item.name}
+            date={item.date}
+            image={item.image}
+            NFTEnabled={item.NFTEnabled}
+            onVerify={() => console.log("Verify:", item.name)}
+          />
+        )}
+      />
     </ScreenBackground>
   );
 };
