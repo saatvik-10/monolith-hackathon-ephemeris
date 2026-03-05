988f89d35f93d29b782d16dbe49a307d7ffddd73
saatvik-10 <saatvikmadan529@gmail.com>
2026-03-05 15:32:16 +0530

    added wallet btn

--- client/components/common/Heading.tsx
+++ client/components/common/Heading.tsx
@@ -1,12 +1,14 @@
-import { View, Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { WalletBtn } from './WalletBtn';
 
-export function Heading({title}: {title: string}) {
+export function Heading({ title }: { title: string }) {
   return (
     <SafeAreaView>
-        <View>
-            <Text className = "font-semibold text-solana-teal pl-4 text-3xl">{title}</Text>
-        </View>
+      <View className="flex-row items-center justify-between">
+        <Text className="pl-4 text-3xl font-semibold text-solana-teal">{title}</Text>
+        <WalletBtn />
+      </View>
     </SafeAreaView>
-  )
+  );
 }
