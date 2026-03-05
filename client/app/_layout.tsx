6378b7c74b888cdb8e2b4ac6b49b9e688b83a2d8
saatvik-10 <saatvikmadan529@gmail.com>
2026-03-05 15:32:34 +0530

    added restore wallet on load

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,14 +1,22 @@
-import "./global.css";
-import { Stack } from "expo-router";
-import GradientBackground from "../components/screen/GradientBackground";
+import './global.css';
+import { Stack } from 'expo-router';
+import GradientBackground from '../components/screen/GradientBackground';
+import { useWalletStore } from '@/components/store/walletStore';
+import { useEffect } from 'react';
 
 export default function RootLayout() {
+  const restoreWallet = useWalletStore((st) => st.restoreWallet);
+
+  useEffect(() => {
+    restoreWallet();
+  }, [restoreWallet]);
+
   return (
     <GradientBackground>
       <Stack
         screenOptions={{
           headerShown: false,
-          contentStyle: { backgroundColor: "transparent" },
+          contentStyle: { backgroundColor: 'transparent' },
         }}
       />
     </GradientBackground>
