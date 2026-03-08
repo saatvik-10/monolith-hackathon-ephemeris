68353efed5e5446d5ec0bb8ccfe65da2628e447c
saatvik-10 <saatvikmadan529@gmail.com>
2026-03-08 19:19:25 +0530

    updated event validator

--- server/src/validators/event.validator.ts
+++ server/src/validators/event.validator.ts
@@ -2,14 +2,15 @@ import { z } from 'zod';
 
 export const eventSchema = z.object({
   name: z.string().min(3, 'Event name should be atleast 3 characters long'),
-  image: z.url('Event image must be a valid URL'),
-  startTime: z.iso.datetime('Start time must be ISO datetime'),
-  startDate: z.date('Event date is required'),
-  endTime: z.iso.datetime("End time must be ISO datetime"),
+  image: z.string('Event image is required'),
+  description: z.string('Event description is required'),
+  startTime: z.string().datetime('Start time must be ISO datetime'),
+  startDate: z.string('Event date is required'),
+  endTime: z.string().datetime('End time must be ISO datetime'),
   location: z.string('Event location is required'),
   locationURL: z.url('Event location URL must be provided'),
   organizerName: z.string(),
-  organizerWallet: z.string("Please connect wallet to create an event"),
+  organizerWallet: z.string('Please connect wallet to create an event'),
   nftEnabled: z.boolean().optional().default(false),
 });
 
