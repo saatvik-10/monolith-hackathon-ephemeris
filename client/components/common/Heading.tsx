c42a0b983bbc68cb735ca57db9614154d28e8e8d
saatvik-10 <saatvikmadan529@gmail.com>
2026-03-08 13:28:14 +0530

    heading padding fixes

--- client/components/common/Heading.tsx
+++ client/components/common/Heading.tsx
@@ -5,8 +5,8 @@ import { WalletBtn } from './WalletBtn';
 export function Heading({ title }: { title: string }) {
   return (
     <SafeAreaView>
-      <View className="flex-row items-center justify-between">
-        <Text className="pl-4 text-3xl font-semibold text-solana-teal">{title}</Text>
+      <View className="flex-row items-center justify-between px-4">
+        <Text className="text-3xl font-semibold text-solana-teal">{title}</Text>
         <WalletBtn />
       </View>
     </SafeAreaView>
